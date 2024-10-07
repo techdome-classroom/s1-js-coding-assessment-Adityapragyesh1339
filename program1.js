5ac43007a05c94b23e6e4cf977a06b1935ea8fa5
Adityapragyesh1339 <adityapragyesh1704@gmail.com>
2024-10-07 13:01:33 +0530

    solved program1 and program2

--- program1.js
+++ program1.js
@@ -1,7 +1,40 @@
 const getTotalIsles = function (grid) {
 
+  if (grid.length === 0) return 0;
 
-  // write your code here
+    let islandCount = 0;
+
+    const rows = grid.length;
+    const cols = grid[0].length;
+
+  
+    function dfs(r, c) {
+        if (r < 0 || c < 0 || r >= rows || c >= cols || grid[r][c] === 'W') {
+            return;
+        }
+
+
+        grid[r][c] = 'W';
+
+       
+        dfs(r + 1, c); 
+        dfs(r - 1, c); 
+        dfs(r, c + 1); 
+        dfs(r, c - 1); 
+    }
+
+
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < cols; c++) {
+            if (grid[r][c] === 'L') {
+               
+                islandCount++;
+                dfs(r, c);
+            }
+        }
+    }
+
+    return islandCount;
 
 };
 
--- program2.js
+++ program2.js
@@ -1,7 +1,28 @@
-const decodeTheRing = function (s, p) {
+const decodeTheRing = function (message, pattern) {
 
-    // write your code here
+  function dfs(mIndex, pIndex) {
+   
+    if (mIndex === message.length && pIndex === pattern.length) return true;
+    
 
+    if (pIndex === pattern.length) return false;
+
+
+    if (pattern[pIndex] === '*') {
+   
+        return dfs(mIndex, pIndex + 1) || (mIndex < message.length && dfs(mIndex + 1, pIndex));
+    }
+
+
+    if (mIndex < message.length && (pattern[pIndex] === '?' || pattern[pIndex] === message[mIndex])) {
+        return dfs(mIndex + 1, pIndex + 1);
+    }
+
+
+    return false;
+}
+
+return dfs(0, 0);
   };
   
   module.exports = decodeTheRing;
